ee742f9fd1670a5bbe9e0bca7012fe4033e5a336
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-20 11:15:22 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+// 1 
+
+
+// var age=18;
+// alert("I am " + age + " Years Old");
+
+
+
+
+// 2
+
+
+// var birthYear=2002;
+// document.write("My Birth Year is " +birthYear);
+
+
+// 3
+
+// var name="John Doe";
+// var prodcutTitle=5;
+// var storeName="xyzClothing Store";
+// document.write(name + " Ordered " + prodcutTitle + " T-Shirts " + "on "+ storeName  )
+
+
+
+// 4
+
+
+
+// var variableName= first,second,third;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
